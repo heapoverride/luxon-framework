225ba10d743baf9b4cda7e04d6bd3e0f32699e55
Unreal Security <arran.bishop89@aol.com>
2021-06-21 04:05:16 +0300

    This is for the best

--- modules/core/Password.php
+++ modules/core/Password.php
@@ -1,33 +1,29 @@
 <?php
 
     class Password {
-        /**
-         * Gets random base64 encoded salt bytes
-         */
-        public static function salt() {
-            return base64_encode(random_bytes(16));
+        private static $salt_length = 16;
+
+        private static function _hash($password, $salt) {
+            if (strlen(APP_SECRET) === 0) throw new Exception("APP_SECRET must not be empty");
+            return bin2hex($salt) . hash_hmac("sha256", implode("\0", [APP_SECRET, $password]), $salt);
         }
 
         /**
-         * Gets strong password hash for storing login passwords
+         * Hash a plaintext password
          * @param string $password Plaintext password
-         * @param string $salt Base64 encoded salt for this user
          */
-        public static function hash($password, $salt) {
-            if (strlen(APP_SECRET) === 0) throw new Exception('APP_SECRET must not be empty');
-            return hash_hmac('sha256', implode("\0", [APP_SECRET, $password]), base64_decode($salt));
+        public static function hash($password) {
+            return self::_hash($password, random_bytes(self::$salt_length));
         }
 
         /**
-         * Test if given plaintext $password\
-         * hashed with APP_SECRET and user's unique $salt stored in database\
-         * results in expected $hash value
+         * Verify password hash
          * @param string $password Plaintext password user is authenticating with
-         * @param string $salt Base64 encoded salt for this user
          * @param string $hash Password hash that was originally created for this user
          */
-        public static function test($password, $salt, $hash) {
-            return self::hash($password, $salt) === $hash;
+        public static function verify($password, $hash) {
+            $salt = substr($hash, 0, self::$salt_length * 2);
+            return self::_hash($password, hex2bin($salt)) === $hash;
         }
     }
 
