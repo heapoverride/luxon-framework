82d6873c5875c1b2b5d11034e630ccee8eaa4aef
Unreal Security <arran.bishop89@aol.com>
2021-06-21 19:43:22 +0300

    Improved Label's constructor

--- modules/core/Html.php
+++ modules/core/Html.php
@@ -1334,12 +1334,14 @@
 
     /**
      * Defines a text label for `Input` of `Form`
-     * @param string $text
+     * @param string $text Specifies the label's text
+     * @param string $for Specifies the id of the form element the label should be bound to
      */
     class Label extends Element {
-        function __construct($text = null) {
+        function __construct($text = null, $for = null) {
             parent::__construct("label");
             if ($text) parent::add($text);
+            if ($for) parent::set("for", $for);
         }
     }
 
