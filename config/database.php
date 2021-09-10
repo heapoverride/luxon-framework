28af40a5f7c5d52cbf8dba4848f383cfd50b8ce5
Unreal Security <arran.bishop89@aol.com>
2021-09-10 14:30:48 +0300

    Updated

--- config/database.php
+++ config/database.php
@@ -1,11 +1,9 @@
 <?php
 
-    /* Database connection */
-    define('DB_CONNECT', false);
-    define('DB_HOST', '127.0.0.1');
-    define('DB_PORT', 3306);
-    define('DB_USER', '');
-    define('DB_PASS', '');
-    define('DB_NAME', '');
-
-?>
+/* Database connection */
+define('DB_CONNECT', false);
+define('DB_HOST', '127.0.0.1');
+define('DB_PORT', 3306);
+define('DB_USER', '');
+define('DB_PASS', '');
+define('DB_NAME', '');
